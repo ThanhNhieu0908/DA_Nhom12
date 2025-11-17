bce14aaa3649a19c187992f15e171b607130cf11
ThanhNhieu0908 <thanhnhieu09082005@gmail.com>
2025-11-17 09:33:12 +0700

    lớp cạnh

---
+++ DoAnTinHoc_Team12/DoAnTinHoc_Team12/Canh.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnTinHoc_Team12
+{
+    public class Canh<T>
+    {
+        private T? _canhKe;
+        private double _trongSo;
+
+        public Canh() { }
+
+        public Canh(T canhKe, double trongSo)
+        {
+            CanhKe = canhKe;
+            TrongSo = trongSo;
+        }
+        public double TrongSo { get => _trongSo; set => _trongSo = value; }
+        public T? CanhKe { get => _canhKe; set => _canhKe = value; }
+    }
+}
